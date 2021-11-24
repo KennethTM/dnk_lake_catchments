38e8bfc759e7881ab23eac55637a4826ae9ef40a
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2021-11-24 15:21:59 +0100

    Updated basin and basin buffered post processing.

--- flowdirgrids.R
+++ flowdirgrids.R
@@ -5,13 +5,6 @@ basins_grass_buffer <- st_read(gis_database, "basins_grass_buffer")
 lakes_grass <- st_read(gis_database, "lakes_grass")
 dk_border <- st_read(gis_database, "dk_border")
 
-basins_grass_buffer_coastline <- basins_grass_buffer %>% 
-  st_intersection(st_geometry(dk_border)) %>% 
-  st_cast("MULTIPOLYGON") %>% 
-  st_make_valid()
-
-st_write(basins_grass_buffer_coastline, gis_database, layer = "basins_grass_buffer_coastline", delete_layer = TRUE)
-
 #Write lake and basin shapefiles to sub-folders
 basin_ids <- sort(unique(lakes_grass$basin_grass_id))
 
@@ -20,17 +13,25 @@ lapply(basin_ids, function(id){
     filter(basin_grass_id == id) %>%
     st_write(paste0(lakes_sub_path, "basin_lakes_", id, ".shp"), delete_dsn = TRUE)
 
-  basins_grass_buffer_coastline %>%
+  basins_grass_buffer %>%
     filter(basin_grass_id == id) %>%
     st_write(paste0(basin_sub_path, "basin_", id, ".shp"), delete_dsn = TRUE)
 })
 
 #Cut dems from buffered basin polygons
 for(i in basin_ids){
+  
+  if(file.exists(paste0(flowdir_sub_path, "basin_", i, ".tif"))){
+    print(paste0("Skipping basin ", i))
+    next
+  }
+  
+  print(paste0("Basin ", i))
+  
   gdalwarp(srcfile = dhym,
            dstfile = paste0(flowdir_sub_path, "basin_", i, ".tif"),
            cutline = gis_database,
-           cl = "basins_grass_buffer_coastline",
+           cl = "basins_grass_buffer",
            cwhere = paste0("basin_grass_id='", i, "'"),
            crop_to_cutline = TRUE,
            overwrite = TRUE,
@@ -42,34 +43,33 @@ for(i in basin_ids){
 }
 
 for(i in basin_ids){
-  if(file.exists(paste0(flowdir_sub_path, "basin_", i, "-breach-flowdirs-lzw.tif"))){
-    next
+  
+  rd_breach_call <- paste(paste0(richdem_apps_path, "rd_depressions_breach.exe"),
+                          paste0(flowdir_sub_path, "basin_", i, ".tif"),
+                          paste0(flowdir_sub_path, "basin_", i, "-breach.tif"),
+                          "COMPLETE NOEPS NOFILL 0 0")
+  
+  if(!file.exists(paste0(flowdir_sub_path, "basin_", i, "-breach.tif"))){
+    print(paste0("Breaching for basin ", i))
+    system(rd_breach_call)
   }
   
-  print("Breaching...")
-  rd_breach_call <- paste0(flowdir_sub_path, "rd_depressions_breach.exe ",
-                           paste0(flowdir_sub_path, "basin_", i, ".tif"),
-                           " ",
-                           paste0(flowdir_sub_path, "basin_", i, "-breach.tif"),
-                           " COMPLETE NOEPS NOFILL 0 0")
-  system(rd_breach_call)
-  
-  print("Fill and flowdirection...")
-  rd_flowdir_call <- paste0(flowdir_sub_path, "rd_d8_flowdirs.exe ", 
+  print(paste0("Fill and flowdirection for basin ", i))
+  rd_flowdir_call <- paste(paste0(richdem_apps_path, "rd_d8_flowdirs.exe"), 
                             paste0(flowdir_sub_path, "basin_", i, "-breach.tif"),
-                            " ",
                             paste0(flowdir_sub_path, "basin_", i, "-breach"))
   system(rd_flowdir_call)
   
 }
 
 for(i in basin_ids){
-  print("Compressing...")
+  
+  print(paste0("Compressing for basin ", i))
   gdal_translate(src_dataset = paste0(flowdir_sub_path, "basin_", i, "-breach-flowdirs.tif"),
                  dst_dataset = paste0(flowdir_sub_path, "basin_", i, "-breach-flowdirs-lzw.tif"),
                  co = "COMPRESS=LZW")
   
-  print("File cleanup...")
+  print(paste0("File cleanup for basin ", i))
   if(file.exists(paste0(flowdir_sub_path, "basin_", i, "-breach-flowdirs-lzw.tif"))){
     file.remove(paste0(flowdir_sub_path, "basin_", i, ".tif"),
                 paste0(flowdir_sub_path, "basin_", i, "-breach.tif"),
--- grass_gis.R
+++ grass_gis.R
@@ -1,5 +1,7 @@
 source("libs_and_funcs.R")
 
+dk_border <- st_read(gis_database, "dk_border")
+
 dhym_10m_labels <- paste0(getwd(), "/data/dhym_10m_labels.tif")
 
 dhym_labels_raster <- raster(dhym_10m_labels)
@@ -11,7 +13,7 @@ link2GI::linkGRASS7(dhym_labels_raster,
                     default_GRASS7 = grass_path,
                     gisdbase = paste0(getwd(), "/grass_database"), 
                     location = "dhym_10m",
-                    gisdbase_exist = FALSE)
+                    gisdbase_exist = TRUE)
 
 #Import dem
 execGRASS("r.in.gdal", flags = c("overwrite", "o"), parameters = list(input = dhym_10m_labels, output = "labels"))
@@ -35,15 +37,36 @@ grass_basins <- readVECT("labels_clean") %>%
   st_set_crs(dk_epsg) %>% 
   mutate(basin_grass_id = 1:n()) %>% 
   select(basin_grass_id) %>% 
-  st_make_valid()
+  st_intersection(dk_border) %>% 
+  st_cast("MULTIPOLYGON") %>%
+  st_make_valid() %>% 
+  select(basin_grass_id)
 
+#Missing danish islands due to erased polygons
+grass_basins_union <- st_union(grass_basins)
+
+dk_border_split <- dk_border %>% 
+  select(GEOMETRY) %>% 
+  st_cast("POLYGON")
+
+dk_border_missing <- dk_border_split[grass_basins_union, , op = st_disjoint] %>% 
+  mutate(basin_grass_id = 1000+(1:n())) %>% 
+  rename(geom = GEOMETRY) %>% 
+  st_cast("MULTIPOLYGON")
+
+#buffer around basins (not islands, those are joined on later)
 grass_basins_buffer <- grass_basins %>%
   st_buffer(1000) %>%
+  st_intersection(dk_border) %>% 
   st_cast("MULTIPOLYGON") %>%
-  st_make_valid()
+  st_make_valid() %>% 
+  select(basin_grass_id)
 
-st_write(grass_basins, gis_database, layer = "basins_grass", delete_layer = TRUE)
-st_write(grass_basins_buffer, gis_database, layer = "basins_grass_buffer", delete_layer = TRUE)
+grass_basins_all <- rbind(grass_basins, dk_border_missing)
+grass_basins_buffer_all <- rbind(grass_basins_buffer, dk_border_missing)
+
+st_write(grass_basins_all, gis_database, layer = "basins_grass", delete_layer = TRUE)
+st_write(grass_basins_buffer_all, gis_database, layer = "basins_grass_buffer", delete_layer = TRUE)
 
 #Read lake polygons
 lakes <- st_read(paste0(rawdata_path, "DK_StandingWater.gml"))
@@ -57,7 +80,7 @@ lakes_clean <- lakes %>%
 #Join basins ids to lakes
 lakes_centroid_basins_id <- lakes_clean %>%
   st_centroid() %>%
-  st_join(grass_basins) %>%
+  st_join(grass_basins_all) %>%
   st_drop_geometry()
 
 lakes_clean_basin_grass_id <- lakes_clean %>%
@@ -70,12 +93,7 @@ st_write(lakes_clean_basin_grass_id, gis_database, "lakes_grass", delete_layer =
 
 
 
-
-dk_border %>% 
-  st_cast("POLYGON") %>% 
-  mutate(area=as.numeric(st_area(geometry))) %>% 
-  filter(area < 10^8)
-
 # missing_lakes <-lakes_clean %>%
 #   left_join(lakes_centroid_basins_id) %>%
 #   filter(is.na(basin_grass_id))
+# mapview::mapview(missing_lakes)
