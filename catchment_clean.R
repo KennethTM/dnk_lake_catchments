9a7b45c8f3a10e41133a145e9653f1361cf59dfe
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2021-12-08 15:51:12 +0100

    Started processing delineated catchment polygons.

--- catchment_clean.R
+++ catchment_clean.R
@@ -1,5 +1,7 @@
 source("libs_and_funcs.R")
+
 library(rmapshaper)
+library(nngeo)
 
 #Lakes
 lakes <- st_read(gis_database, "lakes_grass")
@@ -12,6 +14,8 @@ catchments_path <- list.files(paste0(getwd(), "/data/catchments_sub"), full.name
 
 catch_list <- lapply(catchments_path, st_read)
 
+catch_list_simple <- lapply(catch_list, function(sf){ms_simplify(sf, keep = 0.1, keep_shapes = TRUE, snap = TRUE, sys = TRUE)})
+
 #Add gml_id, remove some duplicate rows and write to database
 catch_all <- do.call(rbind, catch_list) %>% 
   left_join(lakes_gml_ids) %>% 
@@ -22,24 +26,70 @@ catch_all <- do.call(rbind, catch_list) %>%
 
 st_write(catch_all, gis_database, "catchments_raw", delete_layer = TRUE)
 
-# #Simplify and write to database
-# catch_all_simple <- catch_all %>% 
-#   ms_simplify(keep = 0.2, keep_shapes = TRUE, sys = TRUE) %>% 
-#   st_make_valid() %>% 
+catch_all_simple <- do.call(rbind, catch_list_simple) %>% 
+  left_join(lakes_gml_ids) %>% 
+  filter(!duplicated(.)) %>% 
+  arrange(basin_id, lake_id) %>% 
+  st_make_valid() %>% 
+  st_cast("MULTIPOLYGON")
+
+st_write(catch_all_simple, gis_database, "catchments_simple", delete_layer = TRUE)
+
+
+#####
+
+#Remove lake polygon from catchment
+lakes_no_hole <- lakes %>% 
+  st_remove_holes() 
+
+catch_all_no_lake <- ms_erase(catch_all_simple, lakes_no_hole, sys = TRUE)
+
+st_write(catch_all_no_lake, gis_database, "catch_all_no_lake", delete_layer = TRUE)
+
+
+
+
+
+# #Write catchments to .shp file for mapshaper
+# catchments_shp <- paste0(getwd(), "/data/catch_all.shp")
+# 
+# st_write(catch_all, catchments_shp)
+# 
+# #Use mapshaper cmd line functions to simplify polygons
+# catchments_simple_shp <- paste0(getwd(), "/data/catch_all_simple.shp")
+# 
+# simplify_cmd <- paste("mapshaper-xl 8gb", catchments_shp, "snap", "-simplify", "10%", "keep-shapes", "-o", catchments_simple_shp)
+# system(simplify_cmd) 
+# 
+# #Read simplified polygons and write to database
+# catch_all_simple <- st_read(catchments_simple_shp) %>%
+#   st_snap(tolerance = 5) %>% 
+#   st_make_valid() %>%
 #   st_cast("MULTIPOLYGON")
 # 
 # st_write(catch_all_simple, gis_database, "catchments_simple", delete_layer = TRUE)
 
-#save shape, exec in cmd line
-
-####
+#file.remove(c(catchments_shp, catchments_simple_shp))
 
 
-lakes %>% 
-  filter(!(lake_id %in% catch_all$lake_id)) %>% 
-  st_write("lakes_not_delin.sqlite")
+# #catchment without lake
+# left_join(lakes, catch_all_simple) %>% 
+#   slice(1:10) %>% 
+#   mutate(geometry = ifelse(is.na(catch_id), NA, st_difference(geometry1, geometry2)) %>% 
+#   select(-#OLD geoms) %>% 
+#   st_set_geometry(geometry) %>% 
+#   st_cast("MULTIPOLYGON")
 
-#st_buffer
+catch_all <- st_read(gis_database, "catchments_raw")
+catch_all_simple <- st_read(gis_database, "catchments_simple")
+
+
+# lakes_50 <- lakes %>% 
+#   slice(1:10) %>% 
+#   mutate(geometry = st_difference(st_make_valid(st_buffer(st_remove_holes(geometry), 50)), st_make_valid(geometry))) %>% 
+#   st_cast("MULTIPOLYGON")
+
+#st_remove_holes, st_buffer, 
 
 #ms_erase for catchment and buffers
 
