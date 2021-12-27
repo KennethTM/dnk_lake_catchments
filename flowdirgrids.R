758cfcac9f72623197ad61044ee26087c76b2d20
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2021-12-27 22:27:50 +0100

    Changed basin_id column name and re-computed flowdirgrids with -tap gdalwarp flag.

--- flowdirgrids.R
+++ flowdirgrids.R
@@ -6,15 +6,15 @@ lakes_grass <- st_read(gis_database, "lakes_grass")
 dk_border <- st_read(gis_database, "dk_border")
 
 #Write lake and basin shapefiles to sub-folders
-basin_ids <- sort(unique(lakes_grass$basin_grass_id))
+basin_ids <- sort(unique(lakes_grass$basin_id))
 
 lapply(basin_ids, function(id){
   lakes_grass %>%
-    filter(basin_grass_id == id) %>%
+    filter(basin_id == id) %>%
     st_write(paste0(lakes_sub_path, "basin_lakes_", id, ".shp"), delete_dsn = TRUE)
 
   basins_grass_buffer %>%
-    filter(basin_grass_id == id) %>%
+    filter(basin_id == id) %>%
     st_write(paste0(basin_sub_path, "basin_", id, ".shp"), delete_dsn = TRUE)
 })
 
@@ -32,7 +32,7 @@ for(i in basin_ids){
            dstfile = paste0(flowdir_sub_path, "basin_", i, ".tif"),
            cutline = gis_database,
            cl = "basins_grass_buffer",
-           cwhere = paste0("basin_grass_id='", i, "'"),
+           cwhere = paste0("basin_id='", i, "'"),
            crop_to_cutline = TRUE,
            overwrite = TRUE,
            dstnodata = -9999,
@@ -44,6 +44,7 @@ for(i in basin_ids){
            wo = "NUM_THREADS=ALL_CPUS")
 }
 
+#Breach and resolve flowdirs for each basin dem
 for(i in basin_ids){
   
   rd_breach_call <- paste(paste0(richdem_apps_path, "rd_depressions_breach.exe"),
@@ -64,6 +65,7 @@ for(i in basin_ids){
   
 }
 
+#Compress and clean up dem files
 for(i in basin_ids){
   
   print(paste0("Compressing for basin ", i))
@@ -78,19 +80,3 @@ for(i in basin_ids){
                 paste0(flowdir_sub_path, "basin_", i, "-breach-flowdirs.tif"))
   }
 }
-
-
-gdalwarp(srcfile = dhym,
-         dstfile = paste0("basin_47.tif"),
-         cutline = gis_database,
-         cl = "basins_grass_buffer",
-         cwhere = paste0("basin_grass_id='47'"),
-         crop_to_cutline = TRUE,
-         overwrite = TRUE,
-         dstnodata = -9999,
-         co = c("COMPRESS=LZW", "BIGTIFF=YES"),
-         tr = c(1.6, 1.6),
-         tap = TRUE,
-         multi = TRUE,
-         wm = 8000,
-         wo = "NUM_THREADS=ALL_CPUS")
